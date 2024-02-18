59f0d0200e681632f9777fe91e04fb6a8a2ffd92
Pranjal Agrawal <myselfpranjal2005@gmail.com>
2024-02-18 21:19:48 +0530

    add registered user and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const uploadOnCloudinary = async (localFilePath)=> {
+export  const uploadOnCloudinary = async (localFilePath)=> {
     try{
         if(!localFilePath) {
             console.log("Could not find the path");
