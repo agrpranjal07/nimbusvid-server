9549d096f2ce3dec9addc2c581a7df31a03e9724
agrpranjal07 <humesh2258@gmail.com>
2024-03-17 12:22:49 +0530

    Like, Subscription and Tweet controllesrs are added

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,174 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    /*
+    1-Check for valid ChannelId
+    2-If user already subscribed remove it from subsciption database
+    3-If not then add him in subscription database
+    */
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel = await User.findById(channelId)
+
+    if (!channel) {
+        throw new ApiError(404, "This channel does not exist")
+    }
+
+    const loggedInUser = req.user?._id
+    const userSubscribed = await Subscription.findOneAndDelete(
+        { subscriber: loggedInUser, channel: channel }
+    )
+
+    if (userSubscribed) {
+        return res.status(200).json(new ApiResponse(200, "You have been unsubscribed from this channel"))
+    }
+
+    if (!userSubscribed) {
+        const subscription = await Subscription.create({
+            subscriber: loggedInUser,
+            channel,
+        })
+
+        const createdSubscription = await Subscription.findById(subscription._id)
+
+        if (!createdSubscription) {
+            throw new ApiError(500, "something went wrong while subscribing to this channel")
+        }
+
+        return res
+            .status(201)
+            .json(new ApiResponse(200, createdSubscription, "channel subscribed successfully"))
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    
+    /*
+    1- Check for valid channelId
+    2- apply pipeline in Subscriber database and match for channelId
+    3- add subscriber by applying lookup in user(get subscriber details)
+    4- also add total subscriber count 
+    5- send res
+    */
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    if (req.user?._id.toString() != channelId) {
+        throw new ApiError(400, "You are not the owner of this channel to get subscribers list")
+    }
+
+    const getSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $facet: {
+                subscribers: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "subscriber",
+                            foreignField: "_id",
+                            as: "subscriber",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1,
+                                        createdAt: 1,
+                                        updatedAt: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            subscriber: {
+                                $first: "$subscriber"
+                            }
+                        }
+                    }
+                ],
+                subscribersCount: [
+                    { $count: "subscribers" }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200, getSubscribers[0], "All subscribers fetched successfully"))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    /*
+    In above algo(getUserChannelSubscribers) replace subsciber with channel and channel with subscriber
+    */
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber id")
+    }
+
+    if (req.user?._id.toString() != subscriberId) {
+        throw new ApiError(400, "You are not allowed to get channels subscribed by other users. Please use your own subscriber ID")
+    }
+
+    const getSubscribedChannels = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $facet: {
+                channelsSubscribedTo: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "channel",
+                            foreignField: "_id",
+                            as: "channel",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1,
+                                        createdAt: 1,
+                                        updatedAt: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            channel: {
+                                $first: "$channel"
+                            }
+                        }
+                    }
+                ],
+                channelsSubscribedToCount: [
+                    { $count: "channel" }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200, getSubscribedChannels[0], "Channel subscribed by the user fetched successfully"))
+
 })
 
 export {
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,206 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import {Tweet} from "../models/tweet.model.js"
 import {User} from "../models/user.model.js"
+import { Like } from  '../models/like.model.js';
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    /*
+    1-Check for valid content  in the request body
+    2-Create a new Tweet with the user and content
+    3-check if tweet created
+    4-send res
+    */
+    const { content } = req.body
+
+    if (!content || content.trim() === "") {
+        throw new ApiError(400, "Content can't be empty")
+    }
+
+    const user = req.user?._id
+
+    const tweet = await Tweet.create({
+        content,
+        owner: user
+    })
+
+    const createdTweet = await Tweet.findById(tweet._id)
+
+    if (!createdTweet) {
+        throw new ApiError(500, "Something went wrong while creating the tweet in the database")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdTweet, "Your tweet has been posted successfully")
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    /*
+    1-get userId and match for owner id in tweet tweet database
+    2-add total liked, content,owner, created at and updated at to each tweet
+    3-get tweet owner datails
+    4-add tweet and tweet owner details inside TweetListAndOwner
+    3- send res
+    */
+    const { userId } = req.params
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "tweetLikedBy"
+            }
+        },
+        {
+            $group: {
+                _id: "$_id",
+                content: { $first: "$content" },
+                owner: { $first: "$owner" },
+                createdAt: { $first: "$createdAt" },
+                updatedAt: { $first: "$updatedAt" },
+                totalTweetLikes: { $sum: { $size: "$tweetLikedBy" } }
+            }
+        },
+        { $sort: { _id: 1 } }
+    ])
+
+    if (!tweet?.length) {
+        throw new ApiError(404, "Tweet does not exist")
+    }
+    const tweetedBy = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $addFields: {
+                isTweetOwner: {
+                    $cond: {
+                        if: { $eq: [req.user?._id.toString(), userId] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                username: 1,
+                fullName: 1,
+                avatar: 1,
+                createdAt: 1,
+                updatedAt: 1,
+                isTweetOwner: 1
+            }
+        }
+    ])
+
+    const TweetListAndOwner = {
+        tweet,
+        tweetedBy
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, TweetListAndOwner, "tweets fetched successfully"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    
+    /*
+    1-Check for valid content and tweet id in the params and the body
+    2-Check if user is owner of tweet
+    3-Update the tweet
+    4-Send response
+    */
+
+    const { tweetId } = req.params
+    const { content } = req.body
+
+    if (!content || content.trim() === "") {
+        throw new ApiError(400, "Content can't be empty")
+    }
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweetOwner = await Tweet.findById(tweetId)
+
+    if (!tweetOwner) {
+        throw new ApiError(404, "Tweet id does not exist")
+    }
+
+    if (tweetOwner.owner.toString() != req.user?._id.toString()) {
+        throw new ApiError(400, "You are not the owner of this tweet to edit")
+    }
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content: content
+            }
+        },
+        { new: true }
+    )
+
+    return res.status(200).json(new ApiResponse(200, tweet, "tweet updated successfully"))
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    /*
+    1-check for tweet Id and owner
+    2-delete tweet from tweet database and tweet likes from likes database
+    3- send res
+    */
+
+    const { tweetId } = req.params
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "this tweet does not exist")
+    }
+
+    if (tweet.owner.toString() != req.user?._id.toString()) {
+        throw new ApiError(400, "You are not the owner of this tweet to delete")
+    }
+
+    try {
+        await Tweet.findByIdAndDelete(tweetId)
+        // delete likes of deleted tweet
+        await Like.deleteMany({ tweet: tweet._id });
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "tweet deleted successfully"))
+    } catch (error) {
+        throw new ApiError(500, "Unable to delete tweet")
+    }
 })
 
 export {
